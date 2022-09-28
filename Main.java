2d2725b95a8476ff7f2828a6f81bbd127597794f
Maya Strickland <108997905+Angela-Strick@users.noreply.github.com>
2022-09-28 11:24:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+// Author : Maya Strickland
+//Project Name : MCC Grub & Grill
+// Date: 09/27/2022
+
+
+class Main {
+  public static void main(String[] args) {
+    //Welcome Banner for MCC Grub and Grill
+    System.out.println("");
+    System.out.println("Welcome to MCC Grub and Grill!");
+    System.out.println("");
+    // Code to tell the user that the list that is next is our current menu items
+    System.out.println("Here is a list of our current menu items: ");
+    // List of Entree Menu Items Available 
+    System.out.println("");
+    System.out.println("Entrees: ");
+    System.out.println("1. Small Pizza - Perfect size pizza for just one.....$8.99");
+    System.out.println("2. Medium Pizza - A pizza big enough to share, or not.....$11.99");
+    System.out.println("3. Large Pizza - A pizza big enough for the whole family!.....$14.99");
+    System.out.println("4. Veggie Burger - A black bean burger with Tomato, Mayo, Lettuce, and PepperJack Cheese. All grilled on a panini press.....$5.99");
+    System.out.println("5. Grilled Chicken Sandwich - Grilled Chicken topped with Tomato, Mayo, Lettuce, PepperJack Cheese. All grilled on a panini press.....$6.99");
+    System.out.println("6. Hamburger- Classic 100% beef patty with Tomato, Mayo, Lettuce and PepperJack Cheese. All grilled on a panini press");
+    System.out.println("");
+
+    // List of Sides
+    System.out.println("Sides: ");
+    System.out.println("7. Mac and Cheese - Not your momma's mac! Spicy 3 Cheese Mac made with Red Pepper Flakes. Beware the spice!.....$2.49");
+    System.out.println("8. Hand-Cut Fries - Crisp, golden fries deep fried in peanut oil.....$2.75");
+    System.out.println("9. Onion Rings - Crisp, golden rings of onion deep fried in peanut oil.....$2.99");
+    // End of Menu
+    System.out.println("");
+    // Prompts the customer to enter in their selection from 1-9 or 0 to stop ordering food
+    System.out.println("Please select your menu item choice from 1-9 or enter 0 to stop ordering food");
+  }
+}
